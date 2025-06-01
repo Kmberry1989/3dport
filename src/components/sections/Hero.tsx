9a70fb50c12277d29711a015f82bd82f26b47256
Kmberry1989 <kylematthewberry@gmail.com>
2025-06-01 19:43:19 -0400

    ok

--- src/components/sections/Hero.tsx
+++ src/components/sections/Hero.tsx
@@ -7,7 +7,7 @@ const Hero = () => {
   return (
     <section className={`relative mx-auto h-screen w-full`}>
       <div
-        className={`absolute inset-0 top-[120px] mx-auto max-w-7xl ${styles.paddingX} flex flex-row items-start gap-5`}
+        className={`absolute inset-0 top-[112px] mx-auto max-w-7xl ${styles.paddingX} flex flex-row items-start gap-5`}
       >
         <div className="mt-5 flex flex-col items-center justify-center">
           <div className="h-5 w-5 rounded-full bg-[#915EFF]" />
@@ -16,7 +16,8 @@ const Hero = () => {
 
         <div>
           <h1 className={`${styles.heroHeadText} text-white`}>
-            Hi, I'm <span className="text-[#008080]">{config.hero.name}</span>
+         
+            <span className="text-[#008080]">{config.hero.name}</span>
           </h1>
           <p className={`${styles.heroSubText} text-white-100 mt-2`}>
             {config.hero.p[0]} <br className="hidden sm:block" />
