2b4b857925fa4d6b85698324eb39c8cf190676e3
Kmberry1989 <kylematthewberry@gmail.com>
2025-06-03 00:48:21 -0400

    Refactor code structure for improved readability and maintainability

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -24,7 +24,6 @@ import {
   git,
   figma,
   docker,
-  meta,
   carrent,
   jobit,
   tripguide,
@@ -32,9 +31,6 @@ import {
   iuk,
   lhs,
   theatre,
-  shopify,
-  starbucks,
-  tesla,
 } from "../assets";
 
 export const navLinks: TNavLink[] = [
@@ -215,58 +211,6 @@ const experiences: TExperience[] = [
     points: [
       "Managed inventory control for the bar.",
       "Successfully sold many cases of beer before their expiration date, helping to organize goods as the theatre reopened after COVID.",
-    ],
-  },
-  {
-    title: "React.js Developer",
-    companyName: "Starbucks",
-    icon: starbucks,
-    iconBg: "#383E56",
-    date: "March 2020 - April 2021",
-    points: [
-      "Developing and maintaining web applications using React.js and other related technologies.",
-      "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
-      "Implementing responsive design and ensuring cross-browser compatibility.",
-      "Participating in code reviews and providing constructive feedback to other developers.",
-    ],
-  },
-  {
-    title: "React Native Developer",
-    companyName: "Tesla",
-    icon: tesla,
-    iconBg: "#E6DEDD",
-    date: "Jan 2021 - Feb 2022",
-    points: [
-      "Developing and maintaining web applications using React.js and other related technologies.",
-      "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
-      "Implementing responsive design and ensuring cross-browser compatibility.",
-      "Participating in code reviews and providing constructive feedback to other developers.",
-    ],
-  },
-  {
-    title: "Web Developer",
-    companyName: "Shopify",
-    icon: shopify,
-    iconBg: "#383E56",
-    date: "Jan 2022 - Jan 2023",
-    points: [
-      "Developing and maintaining web applications using React.js and other related technologies.",
-      "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
-      "Implementing responsive design and ensuring cross-browser compatibility.",
-      "Participating in code reviews and providing constructive feedback to other developers.",
-    ],
-  },
-  {
-    title: "Full stack Developer",
-    companyName: "Meta",
-    icon: meta,
-    iconBg: "#E6DEDD",
-    date: "Jan 2023 - Present",
-    points: [
-      "Developing and maintaining web applications using React.js and other related technologies.",
-      "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
-      "Implementing responsive design and ensuring cross-browser compatibility.",
-      "Participating in code reviews and providing constructive feedback to other developers.",
     ],
   },
 ];
